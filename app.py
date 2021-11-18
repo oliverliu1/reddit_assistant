20c010f7d362018c97e8dde89b1a19f71898ef25
oliverliu1 <oliverliu100@gmail.com>
2021-11-17 23:01:32 -0600

    Prepare for streamlit deploy

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-import os
 import streamlit as st
 from src.get_text import call_api, parse_text
 from src.text_analyze import summarize
 
-
+'''
+import os
 CLIENT_ID = os.environ.get('CLIENT_ID')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 USERNAME = os.environ.get('REDDIT_USERNAME')
 PASSWORD = os.environ.get('REDDIT_PASSWORD')
+'''
+CLIENT_ID = st.secrets["CLIENT_ID"]
+SECRET_KEY = st.secrets["SECRET_KEY"]
+USERNAME = st.secrets["USERNAME"]
+PASSWORD = st.secrets["PASSWORD"]
+
 
 subreddits = [
               'cscareerquestions',
